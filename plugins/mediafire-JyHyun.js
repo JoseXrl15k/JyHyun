9287e2e2e6aeffac28f7db1f1b732f253b272d91
Jose Elber <xrljose105@gmail.com>
2024-11-26 13:59:20 -0500

    Update mediafire-JyHyun.js

--- plugins/mediafire-JyHyun.js
+++ plugins/mediafire-JyHyun.js
@@ -1,22 +1,31 @@
-/*
-
-JY HYUN MEDIAFIRE
-
-*/
-
+import axios from 'axios'
 import fetch from 'node-fetch'
+import cheerio from 'cheerio'
+import {mediafiredl} from '@bochilteam/scraper'
 
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw m.reply(`Ingresa un link de mediafire\n*✧ Ejemplo:* ${usedPrefix}${command} https://www.mediafire.com/file/2v2x1p0x58qomva/WhatsApp_Messenger_2.24.21.8_beta_By_WhatsApp_LLC.apk/file`);
-conn.sendMessage(m.chat, { react: { text: "🕒", key: m.key } });
-	let ouh = await fetch(`https://api.agatz.xyz/api/mediafire?url=${text}`)
-  let gyh = await ouh.json()
-	await conn.sendFile(m.chat, gyh.data[0].link, `${gyh.data[0].nama}`, `*✧ Nombre:* ${gyh.data[0].nama}\n*✧ Tamaño:* ${gyh.data[0].size}\n*✧ Extensión:* ${gyh.data[0].mime}`, m)
-	await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key }})
-}
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+if (!args[0]) return conn.reply(m.chat, '🚩 Ingrese el enlace de un archivo de Mediafire.', m, rcanal)
+if (!args[0].match(/mediafire/gi)) return conn.reply(m.chat, '🍟 El enlace deve ser de un archivo de Mediafire.', m, rcanal)
+try {
+await m.react(rwait)
+let { title, ext, aploud, size, dl_url } = await mediafiredl(args[0])
+let txt = `乂  *¡MEDIAFIRE - DESCARGAS!*  乂\n\n`
+    txt += `✩ *Nombre* : ${title}\n`
+    txt += `✩ *Peso* : ${size}\n`
+    txt += `✩ *Publicado* : ${aploud}\n`
+    txt += `✩ *MimeType* : ${ext}\n\n`
+    txt += `*- ↻ El archivo se esta enviando espera un momento, soy lento. . .*`
+let img = await (await fetch('https://i.ibb.co/wLQFn7q/logo-mediafire.jpg')).buffer()
+await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, fkontak, null, rcanal)
+await conn.sendFile(m.chat, dl_url, title, null, fkontak, null, { mimetype: ext, asDocument: true })
+await m.react(done)
+} catch (e) {
+await m.reply('☁️ Ocurrió un error inesperado\n\n' + e)
+await m.react(error)
+}}
 handler.help = ['mediafire']
-handler.tags = ['downloader']
-handler.command = /^(mediafire|mf)$/i
+handler.tags = ['descargas']
+handler.command = ['mediafire', 'mdfire', 'mf']
 handler.premium = false
-handler.register = true
+
 export default handler
