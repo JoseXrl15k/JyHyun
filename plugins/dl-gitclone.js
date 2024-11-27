8b6df6269bbd4f55e3b7e5ef517dbb76b3aff0c6
Jose Elber <xrljose105@gmail.com>
2024-11-26 21:36:48 -0500

    Update dl-gitclone.js

--- plugins/dl-gitclone.js
+++ plugins/dl-gitclone.js
@@ -1,20 +1,19 @@
 import fetch from 'node-fetch'
 const regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `${mssg.noLink('GitHub')}\n\n📌 ${mssg.example} : ${usedPrefix + command} https://github.com/JoseXrl15k/JyHyun`
-    if (!regex.test(args[0])) throw `⚠️ ${mssg.noLink('GitHub')}`
-    let [_, user, repo] = args[0].match(regex) || []
-    repo = repo.replace(/.git$/, '')
-    let url = `https://api.github.com/repos/${user}/${repo}/zipball`
-    let filename = (await fetch(url, { method: 'HEAD' })).headers.get('content-disposition').match(/attachment; filename=(.*)/)[1]
-  
-    m.react(rwait)
-    conn.sendFile(m.chat, url, filename, null, m)
-    m.react(done)
-}
-handler.help = ['gitclone <url>']
-handler.tags = ['dl']
+if (!args[0]) return m.reply('🍭 Ingresa el enlace del vídeo de Instagram junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* https://github.com/JoseXrl15k/JyHyun`)
+try {
+if (!regex.test(args[0])) return `La Url es invalida.`
+let [_, user, repo] = args[0].match(regex) || []
+repo = repo.replace(/.git$/, '')
+let url = `https://api.github.com/repos/${user}/${repo}/zipball`
+let filename = (await fetch(url, { method: 'HEAD' })).headers.get('content-disposition').match(/attachment; filename=(.*)/)[1]
+await conn.sendFile(m.chat, url, filename, null, m)
+} catch {
+}}
+handler.help = ['gitclone <url git>']
+handler.tags = ['downloader']
 handler.command = ['gitclone'] 
-handler.diamond = true
-
+handler.register = true 
+//handler.limit = 1
 export default handler
